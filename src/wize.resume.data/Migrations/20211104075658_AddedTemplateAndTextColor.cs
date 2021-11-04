70c9d7721b0f6c1c99c7034197ce6cffbfec301c
Brandon Korous <bkorous@gmail.com>
2021-11-04 00:57:37 -0700

    added migration for template and text color

---
+++ src/wize.resume.data/Migrations/20211104075658_AddedTemplateAndTextColor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace wize.resume.data.Migrations
+{
+    public partial class AddedTemplateAndTextColor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccentTextColor",
+                table: "Resumes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Template",
+                table: "Resumes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccentTextColor",
+                table: "Resumes");
+
+            migrationBuilder.DropColumn(
+                name: "Template",
+                table: "Resumes");
+        }
+    }
+}
