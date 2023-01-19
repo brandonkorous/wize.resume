726634af2814f561b0f75b935995309fd3d08175
Brandon Korous <bkorous@gmail.com>
2023-01-18 16:41:11 -0800

    moved get action to the resume controller

--- src/wize.resume.odata/V1/Controllers/ResumesController.cs
+++ src/wize.resume.odata/V1/Controllers/ResumesController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
 using wize.common.tenancy.Interfaces;
 using wize.resume.data;
 using wize.resume.data.V1;
@@ -12,7 +16,6 @@ using wize.resume.data.V1.Models;
 namespace wize.resume.odata.V1.Controllers
 {
     [ODataRoutePrefix("Resumes")]
-    [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.All, MaxNodeCount = 400, MaxAnyAllExpressionDepth = 2)]
     public partial class ResumesController : BaseODataController<Guid, Resume>
     {
         private readonly WizeContext _context;
@@ -22,6 +25,22 @@ namespace wize.resume.odata.V1.Controllers
         {
             _context = context;
             _tenantProvider = tenantProvider;
+        }
+
+
+        /// <summary>
+        /// OData based GET operation.
+        /// This method will return the requested Dataset.
+        /// </summary>
+        /// <returns>IQueryable of requested type.</returns>
+        [Authorize("list:resume")]
+        [ODataRoute]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.All, MaxNodeCount = 400, MaxAnyAllExpressionDepth = 2)]
+        public override ActionResult<IQueryable<Resume>> Get()
+        {
+            return base.Get();
         }
     }
 }
